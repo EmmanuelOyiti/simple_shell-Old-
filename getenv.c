92c6065ae880c97a0cd9cef9650eb8876cbba9a3
oyiti <emmanueloyiti2@gmail.com>
2023-08-23 18:57:21 -0700

    Updated

--- getenv.c
+++ getenv.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * get_environ - return the string array cpy of our environ
+ * @info: Struct containing probable arguments. 
+ * 	Maintains constant func prototype.
+ * Return: Always retruns 0
  */
 char **get_environ(info_t *info)
 {
@@ -18,11 +18,11 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ * _unsetenv - Deletes an environ variable
+ * @info: Struct containing probable arguments.
+ *	Maintains constant function prototype.
+ *  Return: returns 1 on delete, else 0
+ * @var: the string env var propty
  */
 int _unsetenv(info_t *info, char *var)
 {
@@ -50,13 +50,13 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * _setenv - Init a new environ variable,
+ *             or change an existing one
+ * @info: Struct containing potential arguments. Maintains
  *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
+ * @var: reps string env var propty
+ * @value: reps string env var valued
+ *  Return: Always returns 0
  */
 int _setenv(info_t *info, char *var, char *value)
 {
